18e1463ae0a8fa1d912a9e458d35e37a1c1b79a3
Bob Somers <bob@bobsomers.com>
2014-08-24 06:10:40 -0700

    Compiles on Mac.

--- src/app/module.mk
+++ src/app/module.mk
@@ -2,15 +2,17 @@ $(eval $(new-module))
 
 ifeq "$(platform)" "Darwin"
   CPPREACT_PATH ?= /Users/bsomers/cpp.react
+  glfw_lib := -lglfw3 -framework OpenGL
 else ifeq "$(platform)" "Linux"
   CPPREACT_PATH ?= /home/bsomers/cpp.react
+  glfw_lib := -lglfw -lGL
 else
   $(error Platform not supported)
 endif
 
 module_cppflags := -I$(CPPREACT_PATH)/include
 module_ldflags := -L$(CPPREACT_PATH)/build/lib
-module_ldlibs  := -lCppReact -ltbb -lGLEW -lglfw -lGL
+module_ldlibs  := -lCppReact -ltbb -lGLEW $(glfw_lib)
 
 module_src := $(call glob,*.cpp)
 $(eval $(call static-dep,thermite))
