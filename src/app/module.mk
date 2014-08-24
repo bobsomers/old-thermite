86ced7bc186b7026d4ed9a7d22cfce495754e668
Bob Somers <bob@bobsomers.com>
2014-08-24 05:48:26 -0700

    Working on Mac build, stuck because CppReact won't build.

--- src/app/module.mk
+++ src/app/module.mk
@@ -1,6 +1,12 @@
 $(eval $(new-module))
 
-CPPREACT_PATH ?= /home/bsomers/cpp.react
+ifeq "$(platform)" "Darwin"
+  CPPREACT_PATH ?= /Users/bsomers/cpp.react
+else ifeq "$(platform)" "Linux"
+  CPPREACT_PATH ?= /home/bsomers/cpp.react
+else
+  $(error Platform not supported)
+endif
 
 module_cppflags := -I$(CPPREACT_PATH)/include
 module_ldflags := -L$(CPPREACT_PATH)/build/lib
