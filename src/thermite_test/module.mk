aab2539f0099ea0f02eb1d564cdd3777fbee5113
Bob Somers <bob@bobsomers.com>
2014-08-24 21:56:54 -0700

    Woops.

--- src/thermite_test/module.mk
+++ src/thermite_test/module.mk
@@ -1,14 +1,8 @@
 $(eval $(new-module))
 
 ifeq "$(platform)" "Darwin"
-  CXXTEST_PATH ?= /Users/bsomers/cxxtest-4.4
-else ifeq "$(platform)" "Linux"
-  CXXTEST_PATH ?=
-else
-  $(error Platform not supported)
+  module_cppflags += -I/Users/bsomers/cxxtest-4.4
 endif
-
-module_cppflags := -I$(CXXTEST_PATH)
 
 module_test := $(call glob,*.h)
 module_src  := $(build_dir)/$(module_name)/runner.cpp
