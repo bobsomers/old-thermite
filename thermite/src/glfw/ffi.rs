9d2ab8526c5c375f3921217a587d668103e1269c
Bob Somers <bob@bobsomers.com>
2014-09-18 00:03:21 -0700

    Fleshing out preinit error handling.

--- thermite/src/glfw/ffi.rs
+++ thermite/src/glfw/ffi.rs
@@ -1,8 +1,21 @@
 #![allow(bad_style)]
 
-extern crate libc;
+use libc::{c_char, c_int};
 
-use self::libc::{c_char, c_int};
+// C boolean values.
+//pub static FALSE: c_int = 0;
+pub static TRUE: c_int = 1;
+
+// Error codes.
+pub static NOT_INITIALIZED    : c_int = 0x00010001;
+pub static NO_CURRENT_CONTEXT : c_int = 0x00010002;
+pub static INVALID_ENUM       : c_int = 0x00010003;
+pub static INVALID_VALUE      : c_int = 0x00010004;
+pub static OUT_OF_MEMORY      : c_int = 0x00010005;
+pub static API_UNAVAILABLE    : c_int = 0x00010006;
+pub static VERSION_UNAVAILABLE: c_int = 0x00010007;
+pub static PLATFORM_ERROR     : c_int = 0x00010008;
+pub static FORMAT_UNAVAILABLE : c_int = 0x00010009;
 
 type GLFWerrorfun = extern fn(c_int, *const c_char);
 
--- thermite/src/glfw/mod.rs
+++ thermite/src/glfw/mod.rs
@@ -1,20 +1,66 @@
-extern crate libc;
+use libc::{c_char, c_int};
 
 mod ffi;
 mod platform;
 
-extern fn preinit_error_callback(code: libc::c_int, msg: *const libc::c_char) {
-    use std::string::raw;
+// Public API
 
-    let err_code = code as int;
-    let err_msg = unsafe { raw::from_buf(msg as *const u8) };
-
-    fail!("GLFW preinit failure: {}, {}", err_code, err_msg);
+pub enum Error {
+    NotInitialized,
+    NoCurrentContext,
+    InvalidEnum,
+    InvalidValue,
+    OutOfMemory,
+    ApiUnavailable,
+    VersionUnavailable,
+    PlatformError,
+    FormatUnavailable
 }
 
 pub fn init() -> bool {
     unsafe {
-        ffi::glfwSetErrorCallback(Some(preinit_error_callback));
-        ffi::glfwInit() != 0
+        ffi::glfwSetErrorCallback(Some(fail_on_error_callback));
+        ffi::glfwInit() == ffi::TRUE
     }
+}
+
+// Internals
+
+impl Error {
+    fn from_code(err_code: c_int) -> Error {
+        match err_code {
+            ffi::NOT_INITIALIZED => NotInitialized,
+            ffi::NO_CURRENT_CONTEXT => NoCurrentContext,
+            ffi::INVALID_ENUM => InvalidEnum,
+            ffi::INVALID_VALUE => InvalidValue,
+            ffi::OUT_OF_MEMORY => OutOfMemory,
+            ffi::API_UNAVAILABLE => ApiUnavailable,
+            ffi::VERSION_UNAVAILABLE => VersionUnavailable,
+            ffi::PLATFORM_ERROR => PlatformError,
+            ffi::FORMAT_UNAVAILABLE => FormatUnavailable,
+            _ => fail!("Unknown GLFW error code.")
+        }
+    }
+
+    fn to_string(&self) -> &str {
+        match *self {
+            NotInitialized => "NotInitialized",
+            NoCurrentContext => "NoCurrentContext",
+            InvalidEnum => "InvalidEnum",
+            InvalidValue => "InvalidValue",
+            OutOfMemory => "OutOfMemory",
+            ApiUnavailable => "ApiUnavailable",
+            VersionUnavailable => "VersionUnavailable",
+            PlatformError => "PlatformError",
+            FormatUnavailable => "FormatUnavailable"
+        }
+    }
+}
+
+extern fn fail_on_error_callback(err_code: c_int, err_msg: *const c_char) {
+    use std::string::raw;
+
+    let error = Error::from_code(err_code);
+    let message = unsafe { raw::from_buf(err_msg as *const u8) };
+    fail!("GLFW Error: {}, {}", error.to_string(), message);
 }
